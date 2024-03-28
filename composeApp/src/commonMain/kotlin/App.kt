a6b5455cdfd6928433abe7c0c1b242e0000993f6
sayed.masud <sayed.masud@leads-bd.com>
2024-03-28 09:47:15 +0600

    type issue fixed

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -4,19 +4,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import capitakmmdemo.composeapp.generated.resources.Res
-import capitakmmdemo.composeapp.generated.resources.compose_multiplatform
 import com.matkovivan.nav_cupcake.ui.theme.CapitaTheme
 import shell.ColorSelectionViewModel
 import shell.MainScreenBottomBar
 
-@OptIn(ExperimentalResourceApi::class)
 @Composable
 @Preview
 fun App() {
